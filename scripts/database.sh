37a992578296d9877968378007e40ca68135c4fe
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-12-31 23:11:19 +0200

    inital file ./database.sh

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-select choice in CREATE_DB LIST_DB CONNECT_DB DROP_DB
+SCRIPTS_DIR="."
+DB_DIR="./db"
+
+PS3="Choose an option: "
+
+select choice in CREATE_DB LIST_DB CONNECT_DB DROP_DB EXIT
 do
-case $choice in 
-CREATE_DB )
-    echo "creating db"
-    ./create_db.sh
-    ;;
-LIST_DB )
-    echo "listing db"
-    ./list_db.sh
-    ;;
-CONNECT_DB )
-    echo "connecting db"
-    ./connect_db.sh
-    ;;
-DROP_DB )
-    echo "dropping db"
-    ./drop_db.sh
-    ;;
-* )
-    echo "invalid choice"
-esac
+    case $choice in
+
+    CREATE_DB)
+        echo "Creating database..."
+        "$SCRIPTS_DIR/create_db.sh" "$DB_DIR"
+        ;;
+
+    LIST_DB)
+        echo "Listing databases..."
+         "$SCRIPTS_DIR/list_db.sh" "$DB_DIR"
+        ;;
+
+    CONNECT_DB)
+        echo "Connecting to database..."
+         "$SCRIPTS_DIR/connect_db.sh" "$DB_DIR"
+        ;;
+
+    DROP_DB)
+        echo "Dropping database..."
+         "$SCRIPTS_DIR/drop_db.sh" "$DB_DIR"
+        ;;
+
+    EXIT)
+        echo "Exit"
+        break
+        ;;
+    *)
+        echo "Invalid choice, try again"
+        ;;
+    esac
 done
