8627febec45907acb5b8646c67cd990e6a4e8b23
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-01-01 00:39:37 +0200

    add listing functionality

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPTS_DIR="."
-DB_DIR="./db"
+DB_DIR="../db"
 
 PS3="Choose an option: "
 
@@ -9,27 +9,27 @@ select choice in CREATE_DB LIST_DB CONNECT_DB DROP_DB EXIT
 do
     case $choice in
 
-    CREATE_DB)
+    CREATE_DB )
         echo "Creating database..."
         "$SCRIPTS_DIR/create_db.sh" "$DB_DIR"
         ;;
 
-    LIST_DB)
+    LIST_DB )
         echo "Listing databases..."
          "$SCRIPTS_DIR/list_db.sh" "$DB_DIR"
         ;;
 
-    CONNECT_DB)
+    CONNECT_DB )
         echo "Connecting to database..."
          "$SCRIPTS_DIR/connect_db.sh" "$DB_DIR"
         ;;
 
-    DROP_DB)
+    DROP_DB )
         echo "Dropping database..."
          "$SCRIPTS_DIR/drop_db.sh" "$DB_DIR"
         ;;
 
-    EXIT)
+    EXIT )
         echo "Exit"
         break
         ;;
--- scripts/list_db.sh
+++ scripts/list_db.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "from listing db file"
+DB_DIR="$1"
+
+if [[ ! -d "$DB_DIR" ]]; then
+    echo "Database directory doesnt exist"
+    exit 1
+fi
+
+
+echo "===Datatabases===="
+shopt -s nullglob
+
+db_list=("$DB_DIR"/*)
+
+#echo ${#db_list[@]}
+
+if [[ ${#db_list[@]} -eq 0 ]]; then
+    echo "no databases to show"
+else
+    ls "$DB_DIR"
+fi
+exit 0
