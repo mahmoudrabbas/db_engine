b89aed8e128f2ca4f4e045ebab116627a5173a80
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-01-02 23:32:38 +0200

    dropping tables

--- scripts/drop_table.sh
+++ scripts/drop_table.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
 echo "Hello from dropping table"
+
+# DB_DIR="$1"
+
+shopt -s nullglob
+
+tbls=("."/*)
+
+if [[ ${#tbls[@]} -eq 0 ]]; then
+    echo "No tables to drop"
+    exit 0
+fi
+
+
+tables=()
+
+for tbl in ${tbls[@]}
+do
+    [[ -f "$tbl" ]] && tables+=("$(basename "$tbl")")
+done
+
+PS3="Select table to drop: "
+select t in "${tables[@]}";
+do
+    case $t in
+    "") 
+    echo "Invalid choice, try again!"
+    continue
+    ;;
+    *)
+        if [[ ! -f $t ]]
+        then 
+            echo "Table doesnt exist"
+            continue
+        else
+            rm "$t"
+            echo "Table '$t' deleted"
+            break
+        fi
+    esac
+done
